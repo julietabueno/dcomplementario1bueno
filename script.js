43c6f85b5e7873f22a10b793c9fa35321af2b905
julietabueno <bueno.julieta.ok@gmail.com>
2022-06-26 20:52:27 -0300

    le agregue otra condicion

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let i = parseInt(prompt("ingrese un número del 2 al 20 para restar de a 2"));
+let i = parseInt(prompt("Ingrese un número del 2 al 20 para restar de a 2"));
 
 do {
     i = i - 2;
     alert("La cuenta da " + i + " y si le resto 2 más...")
-} while (i >= 2) {
+} while (i >= 2 && i <= 20) {
     alert("Llegamos a 0 :)");
 }

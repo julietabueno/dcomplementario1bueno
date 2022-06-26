4014aa412bcd7400d2263486f37aafd413b677b0
julietabueno <bueno.julieta.ok@gmail.com>
2022-06-26 20:34:55 -0300

    cree un ciclo do ... while para ingresar un numero y que vaya restando de a 2 hasta llegar a 0

---
+++ script.js
@@ -0,0 +1,8 @@
+let i = parseInt(prompt("ingrese un número del 2 al 20 para restar de a 2"));
+
+do {
+    i = i - 2;
+    alert("La cuenta da " + i + " y si le resto 2 más...")
+} while (i >= 2) {
+    alert("Llegamos a 0 :)");
+}

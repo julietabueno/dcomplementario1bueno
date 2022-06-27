b83f9ff3dc0c760fa5b5c5f1d7b91bd71ed9f207
julietabueno <bueno.julieta.ok@gmail.com>
2022-06-26 21:11:54 -0300

    aguegue condicion para que no entre si el numero no esta en el rango solicitado

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 let i = parseInt(prompt("Ingrese un número del 2 al 20 para restar de a 2"));
 
+
 do {
+
+    if (i < 2 || i > 20) {
+        alert("Ingrese un numero válido");
+    }else {
     i = i - 2;
-    alert("La cuenta da " + i + " y si le resto 2 más...")
+    alert("La cuenta da " + i + " y si le resto 2 más...");
+}
+
 } while (i >= 2 && i <= 20) {
     alert("Llegamos a 0 :)");
 }
+
+
